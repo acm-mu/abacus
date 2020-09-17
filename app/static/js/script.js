5aaed2ed63b006cb3bc56913a02b9b1f2aeee8b1
Jl5her <jl5her@yahoo.com>
2020-09-17 00:42:19 -0500

    Change templates from include to inheritance.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -13,17 +13,18 @@ function format(time) {
   return `${date.getHours()}:${minutes}:${seconds}`;
 }
 
-document.querySelectorAll('[time]').forEach((timeElement) => {
-  timeElement.innerText = format(parseInt(timeElement.getAttribute('time'), 10));
-})
+document.addEventListener('DOMContentLoaded', () => {
+  
+  document.querySelectorAll('[time]').forEach((timeElement) => {
+    timeElement.innerText = format(parseInt(timeElement.getAttribute('time'), 10));
+  });
 
-// Markdown Format
-
-let converter = new showdown.Converter();
-
-document.querySelectorAll('.markdown').forEach((e) => {
-  let md = e.innerHTML;
-  md = md.replaceAll('&gt;', '>');
-  md = md.replaceAll('&lt;', '<');
-  e.innerHTML = converter.makeHtml(md);
-})
+  let converter = new showdown.Converter();
+  // Markdown Format
+  document.querySelectorAll('.markdown').forEach((e) => {
+    let md = e.innerHTML;
+    md = md.replaceAll('&gt;', '>');
+    md = md.replaceAll('&lt;', '<');
+    e.innerHTML = converter.makeHtml(md);
+  });
+});
