34d4947bfb7b530432e062bfd150852692080b20
Jack Pfeiffer <=>
2020-10-26 20:34:48 -0500

    migrate to api implementation

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 function formatTime(milliseconds) {
     
     let hours = Math.floor(milliseconds / (1000 * 60 * 60));
+    if (hours < 10) hours = "0" + hours;
     milliseconds %= (1000 * 60 * 60);
 
     let minutes = "" + Math.floor(milliseconds / (1000 * 60));
@@ -10,7 +11,7 @@ function formatTime(milliseconds) {
     let seconds = "" + Math.floor(milliseconds / 1000);
     if (seconds < 10) seconds = "0" + seconds;
 
-    return  `${hours}:${minutes}:${seconds}`;
+    return `${hours}:${minutes}:${seconds}`;
 }
 
 // Any elements that have a `time` attribute with milliseconds will be formmated.
@@ -37,11 +38,18 @@ function convertMarkdown() {
 // With appropriate default values.
 function fillDateInputs() {
   document.querySelectorAll('.date.fields').forEach((dateField) => {
+    if (!dateField.getAttribute('date')) return
     let values = dateField.getAttribute('date').split(' ');
 
     dateField.querySelector('input[type=date]').value = values[0];
     dateField.querySelector('input[type=time]').value = values[1].substring(0, 5); // Cut of seconds
   })
+}
+
+function createElement(html) {
+  let temp = document.createElement('template')
+  temp.innerHTML = html.trim()
+  return temp.content.firstChild
 }
 
 document.addEventListener("DOMContentLoaded", () => {
