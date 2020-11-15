7ffa67bf26c535c392223586c7c8ff462f00cb5a
Jack Pfeiffer <jl5her@yahoo.com>
2020-11-14 19:57:19 -0600

    Countdown ticker for before compeittion.

--- static/js/countdown.js
+++ static/js/countdown.js
@@ -1,40 +1,39 @@
 let startDate, endDate;
 
 document.addEventListener("DOMContentLoaded", () => {
+
   fetch("/api/contest")
     .then(res => res.json())
     .then(res => {
       document.querySelector("#competition_name").innerText = res.competition_name
       startDate = new Date((parseInt(res.start_date) * 1000) - ((new Date()).getTimezoneOffset() * 60000))
       endDate = new Date((parseInt(res.end_date) * 1000) - ((new Date()).getTimezoneOffset() * 60000))
-    })
+  
+      document.querySelector("#start_time").innerText = startDate.toLocaleString()
+      document.querySelector("#end_time").innerText = endDate.toLocaleString()
 
-  const start = document.querySelector("#start_time")
-  const end = document.querySelector("#end_time")
+      if (Date.now() >= startDate)
+        setInterval(updateClock, 0, 500)
+    })
+})
+
+function updateClock() {
+  document.querySelector("#countdown_during").style.display = 'block'
 
   const timeElapsed = document.querySelector("#timeElapsed")
   const timeRemaining = document.querySelector("#timeRemaining")
 
-  const progressBar = document.querySelector("#progress-bar")
+  const total = endDate - startDate
+  const now = Date.now()
 
-  setInterval(() => {
-    if (!startDate || !endDate) return
-    
-    start.innerText = startDate.toLocaleString()
-    end.innerText = endDate.toLocaleString()
+  if (endDate - now > 0) {
+    timeElapsed.innerText = formatTime(now - startDate)
+    timeRemaining.innerText = formatTime(endDate - now)
+  } else {
+    timeElapsed.innerText = formatTime(endDate - startDate)
+    timeRemaining.innerText = "Finished"
+  }
 
-    const total = endDate - startDate
-    const now = Date.now()
-    const percent = Math.min((now - startDate) / total, 1)
-
-    if (endDate - now > 0) {
-      timeElapsed.innerText = formatTime(now - startDate)
-      timeRemaining.innerText = formatTime(endDate - now)
-    } else {
-      timeElapsed.innerText = formatTime(endDate - startDate)
-      timeRemaining.innerText = "Finished"
-    }
-
-    progressBar.style.width = `${percent * 100}%`
-  }, 20);
-})
+  const percent = Math.min((now - startDate) / total, 1)
+  document.querySelector("#progress-bar").style.width = `${percent * 100}%`
+}
