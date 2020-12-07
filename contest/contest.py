3abaed3084834c53ecc1b7d85f42727321ffaaec
Jack Pfeiffer <jl5her@yahoo.com>
2020-12-06 19:38:45 -0600

    Fix typo

--- contest/contest.py
+++ contest/contest.py
@@ -1,176 +1,225 @@
-import os, hashlib, boto3, time, hashlib, uuid
+import os
+import hashlib
+import boto3
+import time
+import hashlib
+import uuid
 from flask import session
 from boto3.dynamodb.conditions import Attr
+from .utils import split_csv
+
 
 class ContestService:
-  def __init__(self):
-    self.init_aws()
+    def __init__(self):
+        self.init_aws()
 
-  def init_aws(self):
-    aws_access_key = os.environ.get('AWS_ACCESS_KEY', None)
-    aws_secret_key = os.environ.get('AWS_SECRET_KEY', None)
+    def init_aws(self):
+        r""" Connects application to AWS."""
+        aws_access_key = os.environ.get('AWS_ACCESS_KEY', None)
+        aws_secret_key = os.environ.get('AWS_SECRET_KEY', None)
 
-    if not(aws_access_key) or not(aws_secret_key):
-      self.db = self.s3 = None
+        if not aws_access_key or not aws_secret_key:
+            self.db = self.s3 = None
 
-    self.aws = boto3.Session(
-      aws_access_key_id=aws_access_key, 
-      aws_secret_access_key=aws_secret_key,
-      region_name="us-east-2")
+        self.aws = boto3.Session(
+            aws_access_key_id=aws_access_key,
+            aws_secret_access_key=aws_secret_key,
+            region_name="us-east-2")
 
-    self.db = self.aws.resource('dynamodb')
-    self.s3 = self.aws.resource('s3')
-    self.lmbda = self.aws.client('lambda')
+        self.db = self.aws.resource('dynamodb')
+        self.s3 = self.aws.resource('s3')
+        self.lmbda = self.aws.client('lambda')
 
-  def auth_login(self, form_data):
-    m = hashlib.sha256()
-    m.update(form_data['password'].encode())
-    password = m.hexdigest()
+    def auth_login(self, form_data: dict) -> bool:
+        m = hashlib.sha256()
+        m.update(form_data['password'].encode())
+        password = m.hexdigest()
 
-    for user in self.get_users().values():
-      if user['user_name'] == form_data['user-name']:
-        if user['password'] == password:
-          session['user_id'] = user['user_id']
-          session['user_name'] = user['user_name']
-          session['user_type'] = user['type']
-          session['division'] = user['division']
-          # TODO: Instead these should be retrieved from the /api/contest endpoint via the session_token variable. Do a database lookup for the user with that token.
-          session['session_token'] = uuid.uuid4().hex
+        for user in self.get_users():
+            if user['user_name'] == form_data['user-name']:
+                if user['password'] == password:
+                    session['user_id'] = user['user_id']
+                    session['user_name'] = user['user_name']
+                    session['user_role'] = user['role']
+                    session['division'] = user['division']
+                    # TODO: Instead these should be retrieved from the /api/contest endpoint via the session_token variable. Do a database lookup for the user with that token.
+                    session['session_token'] = uuid.uuid4().hex
 
-          self.db.Table('user').update_item(
-            Key={'user_id': user['user_id']},
-            UpdateExpression=f"SET session_token = :session_token",
-            ExpressionAttributeValues={':session_token': session['session_token']})
-          return True
+                    self.db.Table('user').update_item(
+                        Key={'user_id': user['user_id']},
+                        UpdateExpression=f"SET session_token = :session_token",
+                        ExpressionAttributeValues={':session_token': session['session_token']})
+                    return True
+
+        return False
+
+    def is_logged_in(self) -> bool:
+        if 'user_id' in session:
+            user = self.db.Table('user').scan(FilterExpression=Attr(
+                'user_id').eq(session['user_id']))['Items']
+            if user and 'session_token' in user[0]:
+                return user[0]['session_token'] == session['session_token']
+        return False
+
+    def is_judge(self) -> bool:
+        if self.is_logged_in():
+            return session['user_role'] == "judge"
+        return False
+
+    def is_admin(self) -> bool:
+        if self.is_logged_in():
+            return session['user_role'] == "admin"
+        return False
+
+    def home(self) -> str:
+        if 'user_id' not in session or session['user_role'] == "team":
+            return f"/{session['division']}"
+
+        if session['user_role'] == 'judge':
+            return '/'
+
+        if session['user_role'] == 'admin':
+            return '/admin'
+
+    def get_settings(self) -> dict:
+        settings = self.db.Table('setting').scan()['Items']
+        return {s['key']: s['value'] for s in settings}
+
+    def save_settings(self, settings: dict):
+        """ Uploads updated settings to DynamoDB.
+
+        Args:
+            settings: dictionary object containing all settings
+        """
+        with self.db.Table('setting').batch_writer() as batch:
+            for key, value in settings.items():
+                batch.put_item(
+                    Item={
+                        'key': key,
+                        'value': value
+                    })
+
+    def get_problems(self, **filters) -> list:
+        r"""Retrieves problems from DynamoDB.
         
-    return False
+        Args:
+            filters: Arbitrary kword arguments to use as filters for database scan.
 
-  def is_logged_in(self):
-    if 'user_id' in session:
-      user = self.db.Table('user').scan(FilterExpression=Attr('user_id').eq(session['user_id']))['Items']
-      if user and 'session_token' in user[0]:
-        return user[0]['session_token'] == session['session_token']
-    return False
+        Returns:
+            A list of problem object dictionaries or an empty list if no problems match filters.
+         """
+        filter_expression = " AND ".join(
+            f"#{key} = :{key}" for key in filters.keys())
+        names = {f"#{key}": key for key in filters.keys()}
+        values = {f":{key}": value for key, value in filters.items()}
 
-  def is_judge(self):
-    if self.is_logged_in():
-      return session['user_type'] == "judge"
-    return False
+        if not filters:
+            return self.db.Table('problem').scan()['Items']
 
-  def is_admin(self):
-    if self.is_logged_in():
-      return session['user_type'] == "admin"
-    return False
+        return self.db.Table('problem').scan(
+            FilterExpression=filter_expression,
+            ExpressionAttributeValues=values,
+            ExpressionAttributeNames=names)['Items']
 
-  def home(self):
-    if 'user_id' not in session or session['user_type'] == "team":
-      return f"/{session['division']}"
 
-    if session['user_type'] == 'judge':
-      return '/'
-    
-    if session['user_type'] == 'admin':
-      return '/admin'
-    
-  def get_settings(self):
-    settings = self.db.Table('setting').scan()['Items']
-    return {s['key']: s['value'] for s in settings}
+    def get_submissions(self, **filters) -> list:
+        r"""Retrieves submissions from DynamoDB.
 
-  def save_settings(self, settings):
-    with self.db.Table('setting').batch_writer() as batch:
-      for key, value in settings.items():
-        batch.put_item(
-          Item={
-            'key': key,
-            'value': value
-          })
+        Args:
+            filters: Arbitrary kword arguments to use as filters for database scan.
 
-  def get_problems(self, **kwargs):
-    filter_expression = " AND ".join(f"#{key} = :{key}" for key in kwargs.keys())
-    names = {f"#{key}": key for key in kwargs.keys()}
-    values = {f":{key}": value for key,value in kwargs.items()}
+        Returns:
+            A list of submission object dictionaries or an empty list if no submissions match filters. 
+        """
+        if not filters:
+            return self.db.Table('submission').scan()['Items']
 
-    if kwargs:
-      problems = self.db.Table('problem').scan(
-        FilterExpression=filter_expression,
-        ExpressionAttributeValues=values,
-        ExpressionAttributeNames=names)['Items']
-    else:
-      problems = self.db.Table('problem').scan()['Items']
-    
-    return {problem['id']: problem for problem in problems}
-  
-  def get_submissions(self, **kwargs):
-    filter_expression = " AND ".join(f"#{key} = :{key}" for key in kwargs.keys())
-    names = {f"#{key}": key for key in kwargs.keys()}
-    values = {f":{key}": value for key,value in kwargs.items()}
+        filter_expression = " AND ".join(
+            f"#{key} = :{key}" for key in filters.keys())
+        names = {f"#{key}": key for key in filters.keys()}
+        values = {f":{key}": value for key, value in filters.items()}
 
-    if kwargs:
-      submissions = self.db.Table('submission').scan(
-        FilterExpression=filter_expression,
-        ExpressionAttributeValues=values,
-        ExpressionAttributeNames=names)['Items']
-    else:
-      submissions = self.db.Table('submission').scan()['Items']
-      
-    return {sub['submission_id']: sub for sub in submissions}
+        return self.db.Table('submission').scan(
+            FilterExpression=filter_expression,
+            ExpressionAttributeValues=values,
+            ExpressionAttributeNames=names)['Items']
 
-  def get_users(self, **kwargs):
-    filter_expression = " AND ".join(f"#{key} = :{key}" for key in kwargs.keys())
-    names = {f"#{key}": key for key in kwargs.keys()}
-    values = {f":{key}": value for key,value in kwargs.items()}
+    def get_users(self, **filters) -> list:
+        r"""Retrieves users from DynamoDB.
 
-    if kwargs:
-      users = self.db.Table('user').scan(
-        FilterExpression=filter_expression,
-        ExpressionAttributeValues=values,
-        ExpressionAttributeNames=names)['Items']
-    else:
-      users = self.db.Table('user').scan()['Items']
+        Args:
+            filters: Arbitrary kword arguments to use as filters for database scan.
 
-    return {user['user_id']: user for user in users}
+        Returns:
+            A list of user object dictionaries or an empty list if no users match filters.
 
-  def submit(self, request):
-    language = request.form['language']
-    sub_file = request.files['sub-file']
-    submission_id = uuid.uuid4().hex
+        Example:
+            >>> contest.get_users(role = 'team')
+        """
+        if not filters:
+            return self.db.Table('user').scan()['Items']
 
-    problem_id = request.form['problem-id']
-    problem = [prob for prob in contest.get_problems().values() if prob['problem_id'] == problem_id][0]
+        filter_expression = " AND ".join(
+            f"#{key} = :{key}" for key in filters.keys())
+        names = {f"#{key}": key for key in filters.keys()}
+        values = {f":{key}": value for key, value in filters.items()}
 
-    sub_no = len([sub for sub in self.get_submissions().values() if sub['team_id'] == session['user_id'] and sub['problem_id'] == problem_id])
+        return self.db.Table('user').scan(
+            FilterExpression=filter_expression,
+            ExpressionAttributeValues=values,
+            ExpressionAttributeNames=names)['Items']
 
-    # Upload file to AWS S3 Bucket
-    key = f"{ submission_id }/{ sub_file.filename }"
-    self.s3.Bucket('abacus-submissions').upload_fileobj(sub_file, key)
-    obj = self.s3.Bucket('abacus-submissions').Object(key).get()['Body']
+    def import_users(self, file, replace: bool = False):
+        return
+        # lines = [line.decode('utf-8-sig') for line in file.readlines()]
+        # for i in range(len(lines)):
+        #     items = split_csv(lines[i])
 
-    # Generate sha1 hash for file
-    h = hashlib.sha1()
-    h.update(obj.read())
+    def submit(self, request: dict) -> dict:
+        language = request.form['language']
+        sub_file = request.files['sub-file']
+        submission_id = uuid.uuid4().hex
 
-    file_size = self.s3.ObjectSummary('abacus-submissions', key).size
+        problem_id = request.form['problem-id']
+        problem = contest.get_problems()
+        problem = [prob for prob in contest.get_problems(
+        ) if prob['problem_id'] == problem_id][0]
 
-    item = {
-        'submission_id': submission_id,
-        'sub_no': sub_no,
-        'status': "pending",
-        'runtime': 0,
-        'score': 0,
-        'date': int(time.time()),
-        'language': language,
-        'filename': sub_file.filename,
-        'filesize': file_size,
-        'sha1sum': h.hexdigest(),
-        'team_id': session['user_id'],
-        'problem_id': problem_id,
-        'tests': problem['tests']
-      }
+        sub_no = len([sub for sub in self.get_submissions(
+        ) if sub['team_id'] == session['user_id'] and sub['problem_id'] == problem_id])
 
-    self.db.Table('submission').put_item(
-      Item=item
-    )
-    
-    return item
-  
+        # Upload file to AWS S3 Bucket
+        key = f"{ submission_id }/{ sub_file.filename }"
+        self.s3.Bucket('abacus-submissions').upload_fileobj(sub_file, key)
+        obj = self.s3.Bucket('abacus-submissions').Object(key).get()['Body']
+
+        # Generate sha1 hash for file
+        h = hashlib.sha1()
+        h.update(obj.read())
+
+        file_size = self.s3.ObjectSummary('abacus-submissions', key).size
+
+        item = {
+            'submission_id': submission_id,
+            'sub_no': sub_no,
+            'status': "pending",
+            'runtime': 0,
+            'score': 0,
+            'date': int(time.time()),
+            'language': language,
+            'filename': sub_file.filename,
+            'filesize': file_size,
+            'sha1sum': h.hexdigest(),
+            'team_id': session['user_id'],
+            'problem_id': problem_id,
+            'tests': problem['tests']
+        }
+
+        self.db.Table('submission').put_item(
+            Item=item
+        )
+
+        return item
+
+
 contest = ContestService()
