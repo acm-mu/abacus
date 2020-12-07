ff28b944d83bc707aa4bf9b45bad9a4772f96914
Jack Pfeiffer <jl5her@yahoo.com>
2020-12-06 19:45:51 -0600

    Utilize Flask-Restful

--- contest/api.py
+++ contest/api.py
@@ -1,153 +1,180 @@
-import json, hashlib, uuid, decimal, time
-from datetime import datetime
-from flask import Blueprint, request, session, redirect
-from contest import contest
+import json
+import hashlib
+import uuid
+import decimal
+from flask import request, redirect, session
+from flask_restful import Resource, Api
+from contest import contest, auth_required
 
-api = Blueprint('api_bp', __name__, url_prefix='/api',
-                  template_folder='templates')
 
 def dumps(data):
-  return json.loads(json.dumps(data, sort_keys=True, cls=AbacusEncoder))
+    return json.loads(json.dumps(data, sort_keys=True, cls=AbacusEncoder))
 
-@api.route('/contest', methods=['GET', 'POST'])
-def settings():
-  if request.method == "POST" and contest.is_admin():
-    contest.save_settings(request.form)
 
-  data = contest.get_settings()  
-  if 'user_id' in session:
-    data['current_user'] = session['user_id']
-  
-  return dumps(data)
+class Contest(Resource):
 
-@api.route('/users', methods=['GET', 'PUT', 'POST', 'DELETE'])
-# TODO: Delete all submissions for user
-def users():
-  if request.method == "DELETE":
-    contest.db.Table('user').delete_item(
-      Key={
-        'user_id': request.form['user-id']
-      }
-    )
-  if request.method in ["POST", "PUT"]:
-    if 'sub-file' in request.files:
-      ## TODO: import users from csv
-      return 
+    def get(self):
+        data = contest.get_settings()
+        if 'user_id' in session:
+            data['current_user'] = session['user_id']
+        return dumps(data)
 
-    m = hashlib.sha256()
-    m.update(request.form['password'].encode())
+    def post(self):
+        contest.save_settings(request.form)
+        return dumps(contest.get_settings())
 
-    if request.method == "PUT":
-      contest.db.Table('user').update_item(
-        Key={
-            'user_id': request.form['user-id']
-        },
-        UpdateExpression=f"SET #ty = :type, user_name = :user_name, division = :division, display_name = :display_name, password = :password",
-        ExpressionAttributeValues={
-            ':type': request.form['type'],
-            ':user_name': request.form['user-name'],
-            ':division': request.form['division'],
-            ':display_name': request.form['display-name'],
-            ':password': m.hexdigest()
-        },
-        ExpressionAttributeNames={
-          "#ty": "type"
-        })
 
-    elif request.method == "POST":
-      contest.db.Table('user').put_item(
-        Item={
-          'user_id': uuid.uuid4().hex,
-          'type': request.form['type'],
-          'user_name': request.form['user-name'],
-          'display_name': request.form['display-name'],
-          'division': request.form['division'],
-          'password': m.hexdigest()
-        }
-      )
+class Users(Resource):
 
-  return dumps({K: V for K, V in contest.get_users().items() if V['type'] == "team"})
+    @auth_required
+    def get(self):
+        return dumps(contest.get_users())
+        # return dumps({K: V for K, V in contest.get_users().items() if V['type'] == "team"})
 
-@api.route('/submissions', methods=['GET', 'POST', 'DELETE'])
-def submissions():
-  if request.method == "DELETE":
-    submission_id = request.form['submission_id']
-    contest.db.Table('submission').delete_item(
-      Key={
-        'submission_id': submission_id
-      }
-    )
-    contest.s3.Bucket('abacus-submissions').delete_objects(
-      Delete={
-        'Objects': [{
-          'Key': f"{submission_id}"
-        }]
-      })
+    @auth_required
+    def delete(self):
+        # TODO: Delete all users submissions
+        contest.db.Table('user').delete_item(
+            Key={'user_id': request.form['user-id']})
 
-  if request.method == "POST":
-    return dumps(contest.submit(request))
+    def post(self):
+        if 'csv-file' in request.files:
+            replace = request.form['replace']
+            csv_file = request.files['csv-file']
 
-  return dumps(contest.get_submissions())
+            contest.import_users(csv_file, replace)
+        else:
+            m = hashlib.sha256()
+            m.update(request.form['password'].encode())
 
-@api.route('/submissions/<sub_id>')
-def submission(sub_id):
-  submissions = contest.get_submissions()
-  if sub_id not in submissions:
-    return "Not Found"
-  return dumps(submissions[sub_id])
+            if request.method == "PUT":
+                contest.db.Table('user').update_item(
+                    Key={
+                        'user_id': request.form['user-id']
+                    },
+                    UpdateExpression=f"SET role = :role, user_name = :user_name, division = :division, display_name = :display_name, password = :password",
+                    ExpressionAttributeValues={
+                        ':role': request.form['role'],
+                        ':user_name': request.form['user-name'],
+                        ':division': request.form['division'],
+                        ':display_name': request.form['display-name'],
+                        ':password': m.hexdigest()
+                    })
 
-@api.route('/problems', methods=['GET', 'POST'])
-def problems():
-  if request.method == "POST":
-    tests = {k:v for k,v in request.form.items() if '-in' in k or '-out' in k or '-include' in k}
-    tests = [{'in': tests[f"{n}-in"].replace('\r\n', '\n'), 'out': tests[f"{n}-out"].replace('\r\n', '\n'), 'include': tests[f"{n}-include"]} for n in range(1, int(len(tests) / 2) + 1)]
+            elif request.method == "POST":
+                contest.db.Table('user').put_item(
+                    Item={
+                        'user_id': uuid.uuid4().hex,
+                        'role': request.form['role'],
+                        'user_name': request.form['user-name'],
+                        'display_name': request.form['display-name'],
+                        'division': request.form['division'],
+                        'password': m.hexdigest()
+                    }
+                )
 
-    contest.db.Table('problem').put_item(
-        Item={
-          'problem_id': uuid.uuid4().hex,
-          'id': request.form['id'],
-          'problem_name': request.form['problem-name'],
-          'memory_limit': request.form['memory-limit'],
-          'cpu_time_limit': request.form['cpu-time-limit'],
-          'description': request.form['description'],
-          'tests': tests
-        }
-      )
-  return dumps(contest.get_problems())
 
-@api.route('/problems/<prob_id>', methods=['GET', 'PUT', 'DELETE'])
-# TODO: Delete all submissions for problem
-def problem(prob_id):
-  if request.method == "DELETE":
-    contest.db.Table('problem').delete_item(
-      Key={
-        'problem_id': request.form['problem-id']
-      }
-    )
-    return redirect('/problems')
-    
-  if request.method == "PUT":
-    tests = {k:v for k,v in request.form.items() if '-in' in k or '-out' in k}
-    tests = [{'in': tests[f"{n}-in"].replace('\r\n', '\n'), 'out': tests[f"{n}-out"].replace('\r\n', '\n')} for n in range(1, int(len(tests) / 2) + 1)]
+class Submissions(Resource):
+    def get(self):
+        return dumps(contest.get_submissions())
 
-    contest.db.Table('problem').update_item(
-      Key={
-          'problem_id': request.form['problem-id']
-      },
-      UpdateExpression=f"SET id = :id, problem_name = :name, memory_limit = :mlim, cpu_time_limit = :cpu, description = :desc, tests = :tests",
-      ExpressionAttributeValues={
-          ':id': request.form['id'],
-          ':name': request.form['problem-name'],
-          ':mlim': request.form['memory-limit'],
-          ':cpu': request.form['cpu-time-limit'],
-          ':desc': request.form['description'],
-          ':tests': tests
-      })
-  
-  return dumps(contest.get_problems()[prob_id])
+    def delete(self):
+        submission_id = request.form['submission_id']
+        contest.db.Table('submission').delete_item(
+            Key={
+                'submission_id': submission_id
+            }
+        )
+        contest.s3.Bucket('abacus-submissions').delete_objects(
+            Delete={
+                'Objects': [{
+                    'Key': f"{submission_id}"
+                }]
+            })
+
+    def post(self):
+        return dumps(contest.submit(request))
+
+
+class Submission(Resource):
+    def get(self, sid):
+        submissions = contest.get_submissions()
+        if sid not in submissions:
+            return "Not Found"
+        return dumps(submissions[sid])
+
+
+class Problems(Resource):
+    @auth_required
+    def get(self):
+        problems = contest.get_problems()
+        return dumps(problems)
+
+    def post(self):
+        tests = {k: v for k, v in request.form.items(
+        ) if '-in' in k or '-out' in k or '-include' in k}
+        tests = [{'in': tests[f"{n}-in"].replace('\r\n', '\n'), 'out': tests[f"{n}-out"].replace(
+            '\r\n', '\n'), 'include': tests[f"{n}-include"]} for n in range(1, int(len(tests) / 2) + 1)]
+
+        contest.db.Table('problem').put_item(
+            Item={
+                'problem_id': uuid.uuid4().hex,
+                'id': request.form['id'],
+                'problem_name': request.form['problem-name'],
+                'memory_limit': request.form['memory-limit'],
+                'cpu_time_limit': request.form['cpu-time-limit'],
+                'description': request.form['description'],
+                'tests': tests
+            })
+
+
+class Problem(Resource):
+    def get(self, pid):
+        problems = contest.get_problems(problem_id=pid)
+        return dumps(problems)
+
+    def delete(self, pid):
+        contest.db.Table('problem').delete_item(
+            Key={
+                'problem_id': pid
+            }
+        )
+        return redirect('/problems')
+
+    def put(self, pid):
+        tests = {k: v for k, v in request.form.items()
+                 if '-in' in k or '-out' in k}
+        tests = [{'in': tests[f"{n}-in"].replace('\r\n', '\n'), 'out': tests[f"{n}-out"].replace(
+            '\r\n', '\n')} for n in range(1, int(len(tests) / 2) + 1)]
+
+        contest.db.Table('problem').update_item(
+            Key={
+                'problem_id': request.form['problem-id']
+            },
+            UpdateExpression=f"SET id = :id, problem_name = :name, memory_limit = :mlim, cpu_time_limit = :cpu, description = :desc, tests = :tests",
+            ExpressionAttributeValues={
+                ':id': request.form['id'],
+                ':name': request.form['problem-name'],
+                ':mlim': request.form['memory-limit'],
+                ':cpu': request.form['cpu-time-limit'],
+                ':desc': request.form['description'],
+                ':tests': tests
+            })
+
+
+def register_api(app):
+    api = Api(app)
+
+    api.add_resource(Contest, '/api/contest')
+    api.add_resource(Users, '/api/users')
+    api.add_resource(Submissions, '/api/submissions')
+    api.add_resource(Submission, '/api/submissions/<sid>')
+    api.add_resource(Problems, '/api/problems')
+    api.add_resource(Problem, '/api/problems/<pid>')
+
 
 class AbacusEncoder(json.JSONEncoder):
-  def default(self, o):
-    if isinstance(o, decimal.Decimal):
-      return int(o)
-    return super(AbacusEncoder, self).default(o)
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return int(o)
+        return super(AbacusEncoder, self).default(o)
--- index.py
+++ index.py
@@ -1,40 +1,52 @@
 from flask import Flask, render_template, session, request, redirect
 from contest import contest, api
 from blueprints import *
-import os, binascii
+import os
+import binascii
 
 app = Flask(__name__)
 
-app.register_blueprint(api)
 app.register_blueprint(admin)
 app.register_blueprint(blue)
 app.register_blueprint(gold)
 
+api.register_api(app)
+
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  if request.method == 'POST':
-    if contest.auth_login(request.form):
-      return redirect(contest.home())
+    if request.method == 'POST':
+        if contest.auth_login(request.form):
+            return redirect(contest.home())
 
-  return render_template('login.html')
+    return render_template('login.html')
+
 
 @app.route('/logout')
 def logout():
-  del session['user_id']
-  del session['user_name']
-  if 'admin' in session:
-    del session['admin']
-  return redirect('/')
+    del session['user_id']
+    del session['user_name']
+    del session['session_token']
+    if 'admin' in session:
+        del session['admin']
+    return redirect('/')
+
 
 if __name__ == "__main__":
-  app.jinja_env.globals.update(
-    is_logged_in=contest.is_logged_in,
-    is_judge=contest.is_judge,
-    is_admin=contest.is_admin
-  )
-  app.secret_key = binascii.hexlify(os.urandom(24))
-  app.run(debug=True, host='0.0.0.0', port=80)
+    app.jinja_env.globals.update(
+        is_logged_in=contest.is_logged_in,
+        is_judge=contest.is_judge,
+        is_admin=contest.is_admin)
+
+    app.secret_key = binascii.hexlify(os.urandom(24))
+    app.run(debug=True, host='0.0.0.0', port=80)
