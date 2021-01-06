dc729492c04e3cbf60c6eab909cf16f473d2aacb
Jack Pfeiffer <jl5her@yahoo.com>
2021-01-05 23:16:22 -0600

    Connect scratch accounts and submit problems

--- abacus/blueprints/gold/gold.py
+++ abacus/blueprints/gold/gold.py
@@ -10,7 +10,7 @@ def index():
     return render_template('gold/index.html')
 
 
-@gold.route('/connect', methods=['GET', 'POST'])
+@gold.route('/connect', methods=['POST'])
 @login_required
 def connect():
     if request.method == "POST":
@@ -22,13 +22,12 @@ def connect():
                     ExpressionAttributeValues={
                         ':scratch_username': request.form['scratch_username']
                     })
-                    
-    return render_template('gold/connect_scratch.html')
+        return render_template("gold/connected.html")
 
 
 @gold.route('/submit')
 def submit():
-    return render_template('gold/submit.html', scratch_url=request.args.get('scratch_url', default = ''))
+    return render_template('gold/submit.html', project_id=request.args.get('project_id', default = ''), problems=contest.get_problems(division='gold'))
 
 
 @gold.route('/problems')
@@ -44,3 +43,39 @@ def problem(pid):
 @gold.route('/problems/<pid>/submit')
 def submit_problem(pid):
     return render_template('gold/submit_problem.html', problem_id=pid)
+
+
+@gold.route('/submissions')
+@login_required
+def submissions():
+    submissions = []
+    if contest.getuserinfo('role') == "team":
+        submissions = contest.get_submissions(team_id=contest.getuserinfo('user_id'))
+        for submission in submissions:
+            problem = contest.get_problems(
+                problem_id=submission['problem_id'])[0]
+            submission['problem_id'] = problem['id']
+            submission['prob_name'] = problem['problem_name']
+    else:
+        submissions = contest.get_submissions()
+
+    submissions = sorted(
+        submissions, key=lambda obj: obj['date'], reverse=True)
+    return render_template('gold/submissions.html', submissions=submissions)
+
+
+@gold.route('/submissions/<sid>')
+@login_required
+def submission(sid):
+    submissions = contest.get_submissions(submission_id=sid)
+    if not submissions:
+        return render_template('404.html')
+    submission = submissions[0]
+    if contest.getuserinfo('role') == "team" and submission['team_id'] != contest.getuserinfo('user_id'):
+        return render_template('401.html')
+
+    problem = contest.get_problems(problem_id = submission['problem_id'])[0]
+    submission['prob_id'] = problem['id']
+    submission['prob_name'] = problem['problem_name']
+
+    return render_template('gold/submission.html', submission=submission)
