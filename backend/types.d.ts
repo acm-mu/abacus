78b8df4242c04a17cfdbdb6acdfe85755749bf2c
Jack Pfeiffer <jl5her@yahoo.com>
2021-02-05 23:47:46 -0600

    Standings with node.js

---
+++ backend/types.d.ts
@@ -0,0 +1,63 @@
+type TestType = {
+  in: string;
+  out: string;
+  result: string;
+}
+
+type SubmissionType = {
+  date: number;
+  filename: string;
+  filesize: number;
+  language: string;
+  prob_name: string;
+  problem_id: string;
+  runtime: number;
+  score: number;
+  sha1sum: string;
+  status: string;
+  sub_no: number;
+  submission_id: string;
+  team_id: string;
+  team_name: string;
+  tests: TestType[];
+}
+
+type ProblemScoreType = {
+  num_submissions: number;
+  problem_score: number;
+  solved: boolean;
+  submissions: SubmissionType[];
+}
+
+type ProblemType = {
+  problem_id: string;
+  id: string;
+  problem_name: string;
+  description: string;
+  cpu_time_limit: number;
+  memory_limit: number;
+  tests: TestType[];
+}
+
+type UserType = {
+  user_id: string;
+  role: string;
+  username: string;
+  display_name: string;
+  division: string;
+  scratch_username?: string;
+}
+
+type StandingsUser = {
+  user_id: string;
+  role: string;
+  username: string;
+  display_name: string;
+  division: string;
+  scratch_username?: string;
+  solved: number;
+  time: number;
+  problems: [ProblemScoreType];
+}
+
+export type { StandingsUser, TestType, SubmissionType, ProblemType, UserType, ProblemScoreType }
