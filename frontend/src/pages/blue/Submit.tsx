19b55136b10db80cb6ae04842ebc5a59ac3cd757
Jack Pfeiffer <jl5her@yahoo.com>
2021-02-07 13:23:20 -0600

    Submit react conversion

--- frontend/src/pages/blue/Submit.tsx
+++ frontend/src/pages/blue/Submit.tsx
@@ -1,42 +1,116 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 
 import { Block, Countdown } from '../../components'
-import { Form, Button, Select } from 'semantic-ui-react'
+import { Form, Button } from 'semantic-ui-react'
+import { ProblemType, SubmissionType } from '../../types'
+import { useHistory, useParams } from 'react-router-dom'
+import config from '../../environment'
+import './Submit.scss'
+import { UserContext } from '../../context/user'
 
-const languages = [
-  { key: 'python3', value:'Python 3', text: 'Python 3'},
-  { key: 'java', value:'Java', text: 'Java'},
-  { key: 'c', value:'C', text: 'C'}
+interface Language {
+  key: string;
+  value: string;
+  text: string;
+  file_extension: string;
+}
+const languages: Language[] = [
+  { key: 'python3', value: 'Python 3', text: 'Python 3', file_extension: '.py' },
+  { key: 'java', value: 'Java', text: 'Java', file_extension: '.java' },
+  // { key: 'c', value: 'C', text: 'C' }
 ]
 
-const Submit = (): JSX.Element => (
-  <>
-  <Countdown />
-  <Block size='xs-12'>
-    <h1>Submit a solution to - </h1>
+const Submit = (): JSX.Element => {
+  const { user } = useContext(UserContext)
+  const [problem, setProblem] = useState<ProblemType>()
+  const [language, setLanguage] = useState<Language>()
+  const [file, setFile] = useState<File>()
+  const history = useHistory()
 
-    <Form action="/api/submissions" method="POST" enctype="multipart/form-data" callback="submissionSuccess" async>
-      <input type="hidden" name="problem-id" value="{{ problem_id }}"/>
-      <div id="file_dialog">
-        <div className="message">
-          <b>Drag & drop</b>&nbsp; a file here to upload<br />
-          <i>(Or click and choose file)</i>
-        </div>
-        <input id="sub_files_input" type="file" name="sub-file" />
-      </div>
+  const { problem_id: id } = useParams<{ problem_id: string }>()
+  useEffect(() => {
+    fetch(`${config.API_URL}/problems?id=${id}`)
+      .then(res => res.json())
+      .then(res => {
+        res = Object.values(res)[0] as ProblemType
+        setProblem(res)
+      })
+  }, [])
 
-      <Form.Group widths='equal'>
-        <Form.Field id={'languages'} label='Language'>
-          <Select options={languages} />
-        </Form.Field>
-      </Form.Group>
+  const handleSubmit = async () => {
+    if (!(language && file && problem && user)) return
+    const formData = new FormData()
+    formData.set('file_ext', language.file_extension)
+    formData.set('problem_id', problem.problem_id)
+    formData.set('file', file, file.name)
+    formData.set('language', language.key)
+    formData.set('team_id', user.user_id)
 
-      {/* <div class="ui button">Cancel</div> */}
-      <Button>Cancel</Button>
-      <Form.Button>Submit</Form.Button>
-      {/* <input class="ui button" type="submit" value="Submit"> */}
-    </Form>
-  </Block>
-  </>
-)
+    const res = await fetch(`${config.API_URL}/problems/${problem.problem_id}/submit`, {
+      method: 'POST',
+      body: formData
+    })
+
+    const body: SubmissionType = await res.json()
+
+    if (res.status != 200) alert("An error occurred! Please try again")
+
+    history.push(`/blue/submissions/${body.submission_id}`)
+  }
+
+  const uploadChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event?.target?.files?.length) {
+      const file = event.target.files[0]
+      const ext = file.name.substring(file.name.lastIndexOf("."), file.name.length)
+
+      if (file.size > 1024 * 1024) {
+        alert("File is greater than max size (1024 KB)")
+        event.preventDefault()
+        return
+      }
+
+      for (const language of languages) {
+        if (ext == language.file_extension) {
+          setLanguage(language)
+          setFile(file)
+          return
+        }
+      }
+      alert("File type not supported")
+      event.preventDefault()
+    }
+  }
+  return (
+    <>
+      <Countdown />
+      <Block size='xs-12'>
+        <h1>Submit a solution to {problem?.problem_name} </h1>
+
+        <Form onSubmit={handleSubmit}>
+          <div id="file_dialog">
+            <div className="message">
+              {file ? <>
+                <h3>Your submission will include the following files:</h3>
+                <ul> <li>
+                  {file.name}
+                </li> </ul>
+              </> : <>
+                  <b>Drag & drop</b> a file here to upload<br />
+                  <i>(Or click and choose file)</i>
+                </>}
+            </div>
+            <input id="sub_files_input" type="file" name="sub-file" onChange={uploadChange} />
+          </div>
+
+          <Form.Select inline label='Language' placeholder="Select Language" value={language?.value} options={languages} />
+
+          <Form.Group>
+            <Button>Cancel</Button>
+            <Form.Button primary content="Submit" />
+          </Form.Group>
+        </Form>
+      </Block>
+    </>
+  )
+}
 export default Submit
