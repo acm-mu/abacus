c8deb163568388c7e817827d573f469dcc645624
Jack Pfeiffer <jl5her@yahoo.com>
2021-02-07 13:20:22 -0600

    🧹 Prettier

--- frontend/src/pages/gold/Problem.tsx
+++ frontend/src/pages/gold/Problem.tsx
@@ -10,20 +10,20 @@ const Problem = (): JSX.Element => {
     cpu_time_limit: 0,
     memory_limit: 0
   })
-  
+
   return (
     <>
-    <Block size='xs-9'>
-    <h1>Problem { problem.id }
-    <br /> 
-    {problem.problem_name }</h1>
-    <hr />
+      <Block size='xs-9'>
+        <h1>Problem {problem.id}
+          <br />
+          {problem.problem_name}</h1>
+        <hr />
 
-    <MarkdownView markdown={problem.description} />
-  </Block>
-  <Block size='xs-3'>
-    <div style={{display: 'flex', justifyContent: 'space-evenly'}} >
-      {/* <a 
+        <MarkdownView markdown={problem.description} />
+      </Block>
+      <Block size='xs-3'>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly' }} >
+          {/* <a 
       {% if submissions['status'] == "accepted" or submissions['status'] == "pending" or not(is_logged_in()) %}
       class="icon button ui disabled"
       {% else %}
@@ -33,17 +33,17 @@ const Problem = (): JSX.Element => {
         <i class="upload icon"></i>
         Submit
       </a> */}
-      {/* <a class="icon button ui" id="stats" data-tooltip="Stats" data-position="top center" data-inverted="">
+          {/* <a class="icon button ui" id="stats" data-tooltip="Stats" data-position="top center" data-inverted="">
         <i class="chart bar icon"></i>
         Stats
       </a> */}
-    </div>
-    <p><b>Problem ID:</b> {problem.id }</p>
-    <p><b>CPU Time limit:</b> { problem.cpu_time_limit }</p>
-    <p><b>Memory limit:</b> { problem.memory_limit }</p>
-    <p><b>Download:</b> <a>Sample data files</a></p>
-  </Block>
-  </>
+        </div>
+        <p><b>Problem ID:</b> {problem.id}</p>
+        <p><b>CPU Time limit:</b> {problem.cpu_time_limit}</p>
+        <p><b>Memory limit:</b> {problem.memory_limit}</p>
+        <p><b>Download:</b> <a>Sample data files</a></p>
+      </Block>
+    </>
   )
 }
 
