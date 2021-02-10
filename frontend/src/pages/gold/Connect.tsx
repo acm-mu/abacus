2c84905c499a88c27cce52d69e088ef9a28423f2
Jack Pfeiffer <jl5her@yahoo.com>
2021-02-09 22:02:46 -0600

    Scratch connect

--- frontend/src/pages/gold/Connect.tsx
+++ frontend/src/pages/gold/Connect.tsx
@@ -1,15 +1,41 @@
-import React from "react"
+import React, { useContext, useState } from "react"
 import { Input, Form, Button } from "semantic-ui-react"
 import { Block } from "../../components"
+import { UserContext } from "../../context/user"
+import config from '../../environment'
 
-const Connect = (): JSX.Element => (
-  <Block size='xs-12'>
-    <p>You have not yet attached your Scratch® account to your profile. Please enter it below.</p>
-    <Form action="/gold/connect" method="POST" className="ui action input">
-      <Input placeholder="Scratch Username" />
-      <Button content='Connect' floated='right' type="submit" disabled />
-    </Form>
-  </Block>
-)
+const Connect = (): JSX.Element => {
+  const { user } = useContext(UserContext)
+  const [username, setUsername] = useState('')
+  const [disabled, setDisabled] = useState(true)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value)
+    fetch(`https://api.scratch.mit.edu/users/${event.target.value}`)
+      .then(res => setDisabled(res.status != 200))
+  }
+
+  const handleSubmit = () => {
+    const formData = new FormData()
+    if (!user) {
+      alert("You must be logged in to do that!")
+      return
+    }
+    formData.set('user_id', user.user_id)
+    formData.set('scratch_username', username)
+    fetch(`${config.API_URL}/users`, {
+      method: "PUT",
+      body: formData
+    })
+  }
+  return (
+    <Block size='xs-12' >
+      <p>You have not yet attached your Scratch® account to your profile. Please enter it below.</p>
+      <Form onSubmit={handleSubmit}>
+        <Input placeholder="Scratch Username" onChange={handleChange} action={<Button content='Connect' color='orange' disabled={disabled} />} />
+      </Form>
+    </Block>
+  )
+}
 
 export default Connect
--- frontend/src/pages/gold/Home.tsx
+++ frontend/src/pages/gold/Home.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Block } from '../../components'
+import { UserContext } from '../../context/user'
+import Connect from './Connect'
 
-const Home = (): JSX.Element => (
-  <Block size='xs-12'>
-    <h1>Gold Home</h1>
-  </Block>
-)
+const Home = (): JSX.Element => {
+  const { user } = useContext(UserContext)
+  return (
+    <>
+      {user?.scratch_username ? <></> :
+        <Connect />
+      }
+      <Block size='xs-12'>
+        <h1>Gold Home</h1>
+      </Block>
+    </>
+  )
+}
 
 export default Home
