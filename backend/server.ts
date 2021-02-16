988d233358e3b1cf8b78c6130b77d3696ad01898
Jack Pfeiffer <jl5her@yahoo.com>
2021-02-16 16:04:38 -0600

    Workflows for non master branches (#21)
    * Change zipfile
    
    * Run on push
    
    * Send new submissions data back with lambda
    
    * Use lambda data to update submission after rerun
    
    * Restore workflow to master branch
    
    * Workflows for testing code on non master branches
    
    * Test trigger
    
    * Trigger backend test
    
    * Run frontend tests

--- backend/server.ts
+++ backend/server.ts
@@ -16,16 +16,16 @@ import users from "./api/users";
 const PORT = process.env.PORT || 80;
 
 const app = express();
-const server = createServer(app)
-new Server(server, { serveClient: false })
+const server = createServer(app);
+new Server(server, { serveClient: false });
 
-app.use(cors()) // Enables CORS on all endpoints
-app.use(bodyParser.json()) // Middleware to parse body of requests as JSON
-app.use(fileUpload()) // Middleware for uploading files to express (accessible in req.files)
-app.use(morgan('dev'))
+app.use(cors()); // Enables CORS on all endpoints
+app.use(bodyParser.json()); // Middleware to parse body of requests as JSON
+app.use(fileUpload()); // Middleware for uploading files to express (accessible in req.files)
+app.use(morgan('dev'));
 
-app.use(contest, users, submissions, problems, standings, authlib)
-app.get('/', (_, res) => res.status(200).send(' ')) // Sends 200 OK when AWS EBS pings server for health check
+app.use(contest, users, submissions, problems, standings, authlib);
+app.get('/', (_, res) => res.status(200).send(' ')); // Sends 200 OK when AWS EBS pings server for health check
 
 server.listen(PORT, () => {
   console.log(`🚀 Server is running at :${PORT}`);
