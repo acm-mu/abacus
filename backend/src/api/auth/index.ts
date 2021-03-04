d1e7a2a9f2ee78908364e75bdfa8c8832411d0a4
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-04 11:00:55 -0600

    Auth GET endpoint and fix post

--- backend/src/api/auth/index.ts
+++ backend/src/api/auth/index.ts
@@ -1,9 +1,13 @@
 import { Router } from "express"
 import { checkSchema } from "express-validator"
-import { postAuth, schema } from "./postAuth"
+import { postAuth, schema as postSchema } from "./postAuth"
+import { getAuth } from './getAuth'
+import { isAuthenticated } from "../../abacus/authlib"
 
 const auth = Router()
 
-auth.post('/auth', checkSchema(schema), postAuth)
+auth.get('/auth', isAuthenticated, getAuth)
+auth.post('/auth', checkSchema(postSchema), postAuth)
+
 
 export default auth
