2676245d87f549102f517f3c63dd3e8771498955
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-04 10:59:32 -0600

    AppContext for socket, user, loading, and settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Footer } from "./components";
 import "./App.scss";
@@ -8,34 +8,64 @@ import Admin from "./pages/admin/";
 import Blue from './pages/blue/';
 import Gold from './pages/gold/'
 import Index from './pages'
-import { SocketContext, socket } from './context/socket'
-import { UserContext, reloadFromLocalStorage, saveToLocalStorage } from "./context/user";
-import { UserType } from "./types";
+import { AppContext, AppContextType } from "./AppContext";
+import config from './environment'
+import io from 'socket.io-client';
 
 const App = (): JSX.Element => {
-  const [user, setUser] = useState<UserType | undefined>(reloadFromLocalStorage)
 
-  const value = {
-    user, setUser: (user: UserType | undefined) => {
-      saveToLocalStorage(user)
-      setUser(user)
-    }
+  const [user, setUser] = useState()
+  const [settings, setSettings] = useState()
+  const [loaded, setLoaded] = useState(false)
+
+  const checkAuth = async () => {
+    const res = await fetch(`${config.API_URL}/auth`,
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+      })
+    const user = await res.json()
+    setUser(user)
+    setLoaded(true)
+  }
+
+  const loadSettings = async () => {
+    const response = await fetch(`${config.API_URL}/contest`)
+    const data = await response.json()
+
+    setSettings({
+      ...data,
+      start_date: new Date(parseInt(data.start_date) * 1000),
+      end_date: new Date(parseInt(data.end_date) * 1000)
+    })
+  }
+
+  useEffect(() => {
+    checkAuth()
+    loadSettings()
+  }, [])
+
+  const appContext: AppContextType = {
+    user,
+    setUser,
+    socket: io(config.API_URL),
+    loaded,
+    settings
   }
 
   return (
-    <SocketContext.Provider value={socket}>
-      <UserContext.Provider value={value}>
-        <Router>
-          <Switch>
-            <Route path='/admin' component={Admin} />
-            <Route path="/blue" component={Blue} />
-            <Route path="/gold" component={Gold} />
-            <Route path="/" component={Index} />
-          </Switch>
-        </Router>
-        <Footer />
-      </UserContext.Provider>
-    </SocketContext.Provider >
+    <AppContext.Provider value={appContext} >
+      <Router>
+        <Switch>
+          <Route path='/admin' component={Admin} />
+          <Route path="/blue" component={Blue} />
+          <Route path="/gold" component={Gold} />
+          <Route path="/" component={Index} />
+        </Switch>
+      </Router>
+      <Footer />
+    </AppContext.Provider>
   )
 }
 
---
+++ frontend/src/AppContext.ts
@@ -0,0 +1,20 @@
+import { Settings, User } from "abacus";
+import { createContext, Dispatch, SetStateAction } from "react";
+
+export interface AppContextType {
+  user?: User;
+  setUser: Dispatch<SetStateAction<undefined>>;
+  socket?: SocketIOClient.Socket;
+  settings?: Settings;
+  loaded: boolean;
+}
+
+export const AppContext = createContext<AppContextType>({
+  user: undefined,
+  setUser: () => {
+    return
+  },
+  settings: undefined,
+  socket: undefined,
+  loaded: false
+})
