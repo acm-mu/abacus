9f3cd8db30fe5fa8bb3915e0064a09f10bd1a49d
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-15 18:22:54 -0500

    Cleanup frontend for production

---
+++ frontend/src/pages/gold/Standings.tsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import AppContext from '../../AppContext';
+import { Block, Countdown } from '../../components'
+
+const Standings = (): JSX.Element => {
+  const { settings } = useContext(AppContext);
+
+  if (!settings || new Date() < settings.start_date) {
+    return (
+      <>
+        <Countdown />
+        <Block center size='xs-12'>
+          <h1>Competition not yet started!</h1>
+          <p>Standings will become available when the competition begins, and submissions start rolling in.</p>
+        </Block>
+      </>
+    )
+  }
+  return <> </>
+}
+
+export default Standings
