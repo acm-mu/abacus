1e4b350dd379613fd7fada7881afc4061449a68a
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-16 15:02:48 -0500

    #56 conditionally show nav items if logged in

--- frontend/src/pages/blue/BlueNavigation.tsx
+++ frontend/src/pages/blue/BlueNavigation.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import { Navigation } from "components";
+import AppContext from "AppContext";
 
-const BlueNavigation = (): JSX.Element => (
-  <Navigation className="blue-div">
-    <Menu.Item as={NavLink} exact to="/blue" content="Home" />
-    <Menu.Item as={NavLink} to="/blue/problems" content="Problems" />
-    <Menu.Item as={NavLink} to="/blue/standings" content="Standings" />
-    <Menu.Item as={NavLink} to="/blue/submissions" content="Submissions" />
-    <Menu.Item as={NavLink} to="/blue/clarifications" content="Clarifications" />
-  </Navigation>
-);
+const BlueNavigation = (): JSX.Element => {
+  const { user } = useContext(AppContext);
+
+  return (
+    <Navigation className="blue-div">
+      <Menu.Item as={NavLink} exact to="/blue" content="Home" />
+      {user ? <Menu.Item as={NavLink} to="/blue/problems" content="Problems" /> : <></>}
+      <Menu.Item as={NavLink} to="/blue/standings" content="Standings" />
+      {user ? <Menu.Item as={NavLink} to="/blue/submissions" content="Submissions" /> : <></>}
+      {user ? <Menu.Item as={NavLink} to="/blue/clarifications" content="Clarifications" /> : <></>}
+    </Navigation>
+  )
+}
 
 export default BlueNavigation;
--- frontend/src/pages/gold/GoldNavigation.tsx
+++ frontend/src/pages/gold/GoldNavigation.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
 import { Navigation } from "components";
+import AppContext from "AppContext";
 
-const GoldNavigation = (): JSX.Element => (
-  <Navigation className="gold-div">
-    <Menu.Item as={NavLink} exact to="/gold" content="Home" />
-    <Menu.Item as={NavLink} to="/gold/problems" content="Problems" />
-    <Menu.Item as={NavLink} to="/gold/standings" content="Standings" />
-    <Menu.Item as={NavLink} to="/gold/submissions" content="Submissions" />
-    <Menu.Item as={NavLink} to="/gold/clarifications" content="Clarifications" />
-  </Navigation>
-);
+const GoldNavigation = (): JSX.Element => {
+  const { user } = useContext(AppContext)
+
+  return (
+    <Navigation className="gold-div">
+      <Menu.Item as={NavLink} exact to="/gold" content="Home" />
+      {user ? <Menu.Item as={NavLink} to="/gold/problems" content="Problems" /> : <></>}
+      <Menu.Item as={NavLink} to="/gold/standings" content="Standings" />
+      {user ? <Menu.Item as={NavLink} to="/gold/submissions" content="Submissions" /> : <></>}
+      {user ? <Menu.Item as={NavLink} to="/gold/clarifications" content="Clarifications" /> : <></>}
+    </Navigation>
+  )
+}
 
 export default GoldNavigation;
