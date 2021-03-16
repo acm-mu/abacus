b5cd000a9f30341c723b73c2ad1e9556abde4218
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-16 15:13:06 -0500

    #56 conditionally show nav items based on user (#80)
    * #56 conditionally show nav items if logged in
    
    * Include condition for division/role
    
    * Judges are restricted by division too
    
    * Dont allow submissions before or after competition (#78)

--- frontend/src/pages/blue/BlueNavigation.tsx
+++ frontend/src/pages/blue/BlueNavigation.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { Menu } from "semantic-ui-react";
 import { Navigation } from "components";
+import AppContext from "AppContext";
 
-const BlueNavigation = (): JSX.Element => (
-  <Navigation className="blue-div">
-    <Menu.Item as={NavLink} exact to="/blue" content="Home" />
-    <Menu.Item as={NavLink} to="/blue/problems" content="Problems" />
-    <Menu.Item as={NavLink} to="/blue/standings" content="Standings" />
-    <Menu.Item as={NavLink} to="/blue/submissions" content="Submissions" />
-    <Menu.Item as={NavLink} to="/blue/clarifications" content="Clarifications" />
-  </Navigation>
-);
+const BlueNavigation = (): JSX.Element => {
+  const { user } = useContext(AppContext);
+  const hasAccessTo = () => user?.role == 'admin' || user?.division == 'blue'
+
+  return (
+    <Navigation className="blue-div">
+      <Menu.Item as={NavLink} exact to="/blue" content="Home" />
+      {hasAccessTo() ? <Menu.Item as={NavLink} to="/blue/problems" content="Problems" /> : <></>}
+      <Menu.Item as={NavLink} to="/blue/standings" content="Standings" />
+      {hasAccessTo() ? <Menu.Item as={NavLink} to="/blue/submissions" content="Submissions" /> : <></>}
+      {hasAccessTo() ? <Menu.Item as={NavLink} to="/blue/clarifications" content="Clarifications" /> : <></>}
+    </Navigation>
+  )
+}
 
 export default BlueNavigation;
