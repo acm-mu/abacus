e16ba23a5c62f195e8ad471ed61551975241e4b3
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-18 07:58:48 -0500

    Change up wording, move logic out of return

--- frontend/src/pages/blue/practice/Problems.tsx
+++ frontend/src/pages/blue/practice/Problems.tsx
@@ -10,55 +10,61 @@ interface PracticeProblemsProps {
   submissions: { [key: string]: Submission }
 }
 
-const PracticeProblems = ({ problems, submissions }: PracticeProblemsProps): JSX.Element => <>
-  <Countdown />
-  <Block transparent size='xs-12'>
-    <Breadcrumb>
-      <Breadcrumb.Section active>Practice</Breadcrumb.Section>
-    </Breadcrumb>
-  </Block>
+const PracticeProblems = ({ problems, submissions }: PracticeProblemsProps): JSX.Element => {
+  const showClearButton = Object.values(submissions).length > 0
+  const clearHistory = () => {
+    localStorage.removeItem('submissions')
+    location.reload()
+  }
 
-  <Block size="xs-12">
-    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-      <h1>Practice Problems</h1>
-      {Object.values(submissions).length > 0 ? <div><Button onClick={() => { localStorage.removeItem('submissions'); location.reload() }} content="Clear History" /></div> : <></>}
-    </div>
-    <p>Here you can practice problems from previous year&lsquo;s competitions.</p>
+  return <>
+    <Countdown />
+    <Block transparent size='xs-12'>
+      <Breadcrumb>
+        <Breadcrumb.Section active>Practice</Breadcrumb.Section>
+      </Breadcrumb>
+    </Block>
 
-
-    <Table celled>
-      <Table.Header>
-        <Table.Row>
-          <Table.HeaderCell collapsing></Table.HeaderCell>
-          <Table.HeaderCell>Problem Name</Table.HeaderCell>
-          <Table.HeaderCell>Year</Table.HeaderCell>
-          <Table.HeaderCell>Submissions</Table.HeaderCell>
-          <Table.HeaderCell>Status</Table.HeaderCell>
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        {Object.entries(problems).map(([id, { name, year }]) => {
-          const subs = Object.values(submissions).filter(sub => sub.pid == id)
-          return <Table.Row key={id}>
-            <Table.HeaderCell>{id}</Table.HeaderCell>
-            <Table.Cell>
-              <Link to={`/blue/practice/${id}`}>{name}</Link>
-            </Table.Cell>
-            <Table.Cell>{year}</Table.Cell>
-            <Table.Cell>{subs.length}</Table.Cell>
-            <Table.Cell>{subs.length > 0 ? (() => {
-              switch (subs[subs.length - 1].status) {
-                case 'accepted': return <Label icon='check' color='green' content="Accepted" />
-                case 'rejected': return <Label icon='times' color='red' content="Failed" />
-                case 'pending': return <Label icon='question' content="Pending" />
-                default: return <Label content={subs[subs.length - 1].status} />
-              }
-            })() : <Label content="Unattempted" />}</Table.Cell>
+    <Block size="xs-12">
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <h1>Practice Problems</h1>
+        {showClearButton ? <div><Button onClick={clearHistory} content="Clear History" /></div> : <></>}
+      </div>
+      <p>Here you can practice using our platform with some of the problems from our previous competitions.</p>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell collapsing></Table.HeaderCell>
+            <Table.HeaderCell>Problem Name</Table.HeaderCell>
+            <Table.HeaderCell>Year</Table.HeaderCell>
+            <Table.HeaderCell>Submissions</Table.HeaderCell>
+            <Table.HeaderCell>Status</Table.HeaderCell>
           </Table.Row>
-        })}
-      </Table.Body>
-    </Table>
-  </Block>
-</>
+        </Table.Header>
+        <Table.Body>
+          {Object.entries(problems).map(([id, { name, year }]) => {
+            const subs = Object.values(submissions).filter(sub => sub.pid == id)
+            return <Table.Row key={id}>
+              <Table.HeaderCell>{id}</Table.HeaderCell>
+              <Table.Cell>
+                <Link to={`/blue/practice/${id}`}>{name}</Link>
+              </Table.Cell>
+              <Table.Cell>{year}</Table.Cell>
+              <Table.Cell>{subs.length}</Table.Cell>
+              <Table.Cell>{subs.length > 0 ? (() => {
+                switch (subs[subs.length - 1].status) {
+                  case 'accepted': return <Label icon='check' color='green' content="Accepted" />
+                  case 'rejected': return <Label icon='times' color='red' content="Failed" />
+                  case 'pending': return <Label icon='question' content="Pending" />
+                  default: return <Label content={subs[subs.length - 1].status} />
+                }
+              })() : <Label content="Unattempted" />}</Table.Cell>
+            </Table.Row>
+          })}
+        </Table.Body>
+      </Table>
+    </Block>
+  </>
+}
 
 export default PracticeProblems
