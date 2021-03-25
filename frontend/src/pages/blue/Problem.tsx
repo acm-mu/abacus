c186e162df356d9c81d62cc32a97e6bf04665140
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-24 22:13:04 -0500

    Ask clarification from problem page

--- frontend/src/pages/blue/Problem.tsx
+++ frontend/src/pages/blue/Problem.tsx
@@ -1,12 +1,13 @@
 import { Problem, Submission } from "abacus";
 import React, { useState, useEffect, useContext } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Button, Loader, Popup } from "semantic-ui-react";
+import { Button, Loader } from "semantic-ui-react";
 import MDEditor from "@uiw/react-md-editor";
 import { Block, Countdown, NotFound } from 'components'
 import config from 'environment'
 import AppContext from "AppContext";
 import "./Problem.scss";
+import ClarificationModal from "components/ClarificationModal";
 
 const problem = (): JSX.Element => {
   const { user } = useContext(AppContext);
@@ -60,39 +61,26 @@ const problem = (): JSX.Element => {
     <>
       <Countdown />
       <Block size='xs-9' className='problem'>
-        <h1>
-          Problem {problem?.id}: {problem?.name}
-        </h1>
+        <h1>Problem {problem.id}: {problem.name}</h1>
         <hr />
-        <MDEditor.Markdown source={problem?.description || ''} />
+        <MDEditor.Markdown source={problem.description || ''} />
       </Block>
-      <Block size='xs-3'>
-        <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-          {!submissions || submissions.filter((e) => e.status == "accepted").length == 0 ?
-            <Popup
-              trigger={
-                <Button
-                  as={Link}
-                  to={`/blue/problems/${problem?.id}/submit`}
-                  content="Submit"
-                  icon="upload"
-                />
-              }
-              content="Submit"
-              position="top center"
-              inverted /> : <></>
-          }
-          {/* <Popup
-            trigger={<Button content="Stats" icon="chart bar" />}
-            content="Stats"
-            position="top center"
-            inverted
-          /> */}
-        </div>
-        <p><b>Problem ID:</b> {problem?.id}</p>
-        <p><b>CPU Time limit:</b> {problem?.cpu_time_limit}</p>
-        <p><b>Memory limit:</b> {problem?.memory_limit}</p>
-        <p><b>Download:</b> <a href={`${config.API_URL}/sample_files?pid=${problem?.pid}`}>Sample data files</a></p>
+      <Block size='xs-3' className='problem-panel'>
+        {!submissions || submissions.filter((e) => e.status == "accepted").length == 0 ?
+          <Button
+            as={Link}
+            to={`/blue/problems/${problem?.id}/submit`}
+            content="Submit"
+            icon="upload"
+          /> : <></>
+        }
+        <ClarificationModal title={`${problem.name} | `} trigger={
+          <Button content="Ask" icon="question" />
+        } />
+        <p><b>Problem ID:</b> {problem.id}</p>
+        <p><b>CPU Time limit:</b> {problem.cpu_time_limit}</p>
+        <p><b>Memory limit:</b> {problem.memory_limit}</p>
+        <p><b>Download:</b> <a href={`${config.API_URL}/sample_files?pid=${problem.pid}`}>Sample data files</a></p>
       </Block>
     </>
   );
