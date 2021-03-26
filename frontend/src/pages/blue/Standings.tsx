bf7920667dd913b77eb758596c0efd8530b024e9
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-26 00:51:20 -0500

    Errors with standings

--- frontend/src/pages/blue/Standings.tsx
+++ frontend/src/pages/blue/Standings.tsx
@@ -1,7 +1,7 @@
 import { Problem, ProblemScore, StandingsUser } from "abacus";
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
-import { Loader, Table } from "semantic-ui-react";
+import { Loader, Message, Table } from "semantic-ui-react";
 import { Block, Countdown } from "components";
 import config from 'environment'
 import "./Standings.scss";
@@ -11,30 +11,32 @@ const Standings = (): JSX.Element => {
   const { settings } = useContext(AppContext);
   const [problems, setProblems] = useState<Problem[]>();
   const [standings, setStandings] = useState<StandingsUser[]>();
-  const [loading, setLoading] = useState<boolean>(true)
-  const [isMounted, setMounted] = useState<boolean>(false)
+  const [isLoading, setLoading] = useState<boolean>(true)
+  const [isMounted, setMounted] = useState<boolean>(true)
+
+  const loadData = async () => {
+    let response = await fetch(`${config.API_URL}/problems?division=blue`)
+
+    let problems = Object.values(await response.json()) as Problem[]
+    problems = problems.sort((a: Problem, b: Problem) => a.id.localeCompare(b.id))
+
+    if (!isMounted) return
+    setProblems(problems)
+
+    response = await fetch(`${config.API_URL}/standings`)
+
+    let standings = Object.values(await response.json()) as StandingsUser[]
+    standings = standings.sort((a: StandingsUser, b: StandingsUser) => b.solved - a.solved)
+
+    if (!isMounted) return
+    setStandings(standings)
+
+    setLoading(false)
+
+  }
 
   useEffect(() => {
-    setMounted(true)
-    fetch(`${config.API_URL}/problems?division=blue`)
-      .then((res) => res.json())
-      .then((problems) => {
-        problems = Object.values(problems)
-        problems.sort((a: Problem, b: Problem) => a.id.localeCompare(b.id))
-        if (isMounted)
-          setProblems(problems)
-      });
-
-    fetch(`${config.API_URL}/standings`)
-      .then((res) => res.json())
-      .then((standings) => {
-        standings = Object.values(standings)
-        standings.sort((a: StandingsUser, b: StandingsUser) => b.solved - a.solved)
-        if (isMounted) {
-          setStandings(standings)
-          setLoading(false)
-        }
-      });
+    loadData()
     return () => { setMounted(false) }
   }, []);
 
@@ -49,6 +51,9 @@ const Standings = (): JSX.Element => {
       </>
     )
   }
+
+  if (isLoading) return <Loader active inline='centered' content="Loading" />
+  if (!standings || !problems) return <Message icon='exclamation' error header='Error' content="An error has occurred! Please contact support" />
 
   return (
     <>
@@ -70,69 +75,66 @@ const Standings = (): JSX.Element => {
         </div>
       </Block>
 
-      {loading ?
-        <Loader active inline='centered' content="Loading" /> :
-        <Block transparent size='xs-12'>
-          <Table celled id={"standings"}>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell collapsing>RK</Table.HeaderCell>
-                <Table.HeaderCell>Team</Table.HeaderCell>
-                <Table.HeaderCell collapsing>SLV.</Table.HeaderCell>
-                <Table.HeaderCell collapsing>TIME</Table.HeaderCell>
-                {problems ? problems.map((problem: Problem) => (
-                  <Table.HeaderCell key={problem.id} collapsing>
-                    <Link to={`/blue/problems/${problem.id}`}>{problem.id}</Link>
-                  </Table.HeaderCell>
-                )) : <></>}
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {standings ? standings.map((team: StandingsUser, index: number) => (
-                <Table.Row key={team.uid}>
-                  <Table.Cell collapsing>{index + 1}</Table.Cell>
-                  <Table.Cell>
-                    <Link to="#">{team.display_name}</Link>
-                  </Table.Cell>
-                  <Table.Cell>{team.solved}</Table.Cell>
-                  <Table.Cell>{team.time}</Table.Cell>
-                  {Object.values(team.problems).map(
-                    (problem: ProblemScore, index: number) => {
-                      if (problem.solved) {
-                        return (
-                          <Table.Cell key={`${team.uid}-${index}`} className="solved">
-                            {problem.submissions.length}
-                            <br />
-                            <small>{problem.problem_score}</small>
-                          </Table.Cell>
-                        );
-                      } else if (problem.submissions.length && problem.submissions[problem.submissions.length - 1].status == "pending") {
-                        return (
-                          <Table.Cell key={`${team.uid}-${index}`} className="pending">
-                            {problem.submissions.length}
-                            <br />
+      <Block transparent size='xs-12'>
+        <Table celled id={"standings"}>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell collapsing>RK</Table.HeaderCell>
+              <Table.HeaderCell>Team</Table.HeaderCell>
+              <Table.HeaderCell collapsing>SLV.</Table.HeaderCell>
+              <Table.HeaderCell collapsing>TIME</Table.HeaderCell>
+              {problems.map((problem) => (
+                <Table.HeaderCell key={problem.id} collapsing>
+                  <Link to={`/blue/problems/${problem.id}`}>{problem.id}</Link>
+                </Table.HeaderCell>
+              ))}
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {standings.map((team, index) => (
+              <Table.Row key={team.uid}>
+                <Table.Cell collapsing>{index + 1}</Table.Cell>
+                <Table.Cell>
+                  <Link to="#">{team.display_name}</Link>
+                </Table.Cell>
+                <Table.Cell>{team.solved}</Table.Cell>
+                <Table.Cell>{team.time}</Table.Cell>
+                {Object.values(team.problems).map(
+                  (problem: ProblemScore, index: number) => {
+                    if (problem.solved) {
+                      return (
+                        <Table.Cell key={`${team.uid}-${index}`} className="solved">
+                          {problem.submissions.length}
+                          <br />
+                          <small>{problem.problem_score}</small>
+                        </Table.Cell>
+                      );
+                    } else if (problem.submissions.length && problem.submissions[problem.submissions.length - 1].status == "pending") {
+                      return (
+                        <Table.Cell key={`${team.uid}-${index}`} className="pending">
+                          {problem.submissions.length}
+                          <br />
                             --
-                          </Table.Cell>
-                        );
-                      } else if (problem.submissions.length) {
-                        return (
-                          <Table.Cell key={`${team.uid}-${index}`} className="attempted">
-                            {problem.submissions.length}
-                            <br />
+                        </Table.Cell>
+                      );
+                    } else if (problem.submissions.length) {
+                      return (
+                        <Table.Cell key={`${team.uid}-${index}`} className="attempted">
+                          {problem.submissions.length}
+                          <br />
                             --
-                          </Table.Cell>
-                        );
-                      } else {
-                        return <Table.Cell key={`${team.uid}-${index}`}></Table.Cell>;
-                      }
+                        </Table.Cell>
+                      );
+                    } else {
+                      return <Table.Cell key={`${team.uid}-${index}`}></Table.Cell>;
                     }
-                  )}
-                </Table.Row>
-              )) : <></>}
-            </Table.Body>
-          </Table>
-        </Block>
-      }
+                  }
+                )}
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+      </Block>
     </>
   );
 };
