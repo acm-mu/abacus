2562ff9b2bddc47ab471a6707381267a3956a452
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-26 22:28:09 -0500

    Remove socket and notifications from App Context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,16 @@
-import { Notification, User } from 'abacus';
+import { User } from 'abacus';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import io from 'socket.io-client';
 import { Index, Admin, Blue, Gold } from 'pages'
 import AppContext, { AppContextType } from 'AppContext';
 import config from 'environment'
 import { Footer, Notifications } from 'components';
-import { v4 as uuidv4 } from 'uuid';
 import './App.scss';
 
 const App = (): JSX.Element => {
   const [user, setUser] = useState<User>()
   const [settings, setSettings] = useState()
   const [isLoading, setLoading] = useState(true)
-  const [socket, setSocket] = useState<SocketIOClient.Socket>()
-  const [notifications, setNotifications] = useState<Notification[]>([])
 
   const checkAuth = async () => {
     try {
@@ -43,38 +39,29 @@ const App = (): JSX.Element => {
     return false
   }
 
-  const sendNotification = (notification: Notification) =>
-    setNotifications(notifications =>
-      notifications.concat(notification))
-
   const loadApp = async () => {
     try {
       await loadSettings()
       await checkAuth()
-      setSocket(io(config.API_URL))
     } catch (err) {
-      sendNotification({ id: uuidv4(), type: 'error', header: 'Uh oh!', content: "We are having issues communicating with our servers." })
+      // sendNotification({ id: uuidv4(), type: 'error', header: 'Uh oh!', content: "We are having issues communicating with our servers." })
     }
   }
 
   useEffect(() => {
-    loadApp().then(() => {
-      setLoading(false)
-    })
+    loadApp().then(() => setLoading(false))
   }, [])
 
   const appContext: AppContextType = {
     user,
     setUser,
-    sendNotification,
-    socket,
     settings
   }
 
   if (isLoading) return <></>
 
   return <AppContext.Provider value={appContext}>
-    <Notifications notifications={notifications} setNotifications={setNotifications} />
+    <Notifications />
     <Router>
       <Switch>
         <Route path='/admin' component={Admin} />
--- frontend/src/AppContext.ts
+++ frontend/src/AppContext.ts
@@ -1,20 +1,16 @@
-import { Settings, User, Notification } from 'abacus';
+import { Settings, User } from 'abacus';
 import { createContext, Dispatch, SetStateAction } from 'react';
 
 export interface AppContextType {
   user?: User;
   setUser: Dispatch<SetStateAction<User | undefined>>;
-  sendNotification: (notification: Notification) => void;
-  socket?: SocketIOClient.Socket;
   settings?: Settings;
 }
 
 const AppContext = createContext<AppContextType>({
   user: undefined,
-  sendNotification: () => { return },
   setUser: () => { return },
   settings: undefined,
-  socket: undefined
 })
 
 export default AppContext
