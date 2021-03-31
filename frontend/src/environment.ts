c1f2fa8bd6b1f759ee375ae5b1c5aa993630f086
Jack Pfeiffer <jl5her@yahoo.com>
2021-03-31 15:40:01 -0500

    Lambda cleanup (#125)
    * Remove old and unused lambdas
    
    * Staging detection
    
    * environmentText & Yarn Cache
    
    * Remove old and unused lambdas (#123)
    
    Co-authored-by: Brad Cooley <brad@cooleyweb.org>

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,21 +1,25 @@
 interface Config {
   isLocal: boolean;
   API_URL: string;
+  environmentText: string;
 }
 
 const prod: Config = {
   isLocal: false,
-  API_URL: 'https://api.codeabac.us'
+  API_URL: 'https://api.codeabac.us',
+  environmentText: ''
 }
 
 const dev: Config = {
   isLocal: true,
-  API_URL: 'http://localhost'
+  API_URL: 'http://localhost',
+  environmentText: 'LOCAL ENV.'
 }
 
 const staging: Config = {
   isLocal: true,
-  API_URL: 'https://api-staging.codeabac.us'
+  API_URL: 'https://api-staging.codeabac.us',
+  environmentText: 'STAGING ENV.'
 }
 
 const isStaging = window.location.hostname.startsWith('staging')
