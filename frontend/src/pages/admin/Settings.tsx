d2cb3ba8015bf65da4ed5353f9507c9c57ddc196
Jack Pfeiffer <jl5her@yahoo.com>
2021-04-15 03:39:39 -0500

    Natural order, show practice before comp

--- frontend/src/pages/admin/Settings.tsx
+++ frontend/src/pages/admin/Settings.tsx
@@ -123,18 +123,6 @@ const Settings = (): JSX.Element => {
           name="practice_name"
         />
 
-        <h3>Competition Time</h3>
-
-        <Form.Group widths='equal'>
-          <Form.Field label='Start Date' control={Input} type='date' onChange={handleChange} name='start_date' value={settings.start_date} />
-          <Form.Field label='Start Time' control={Input} type='time' onChange={handleChange} name='start_time' value={settings.start_time} />
-        </Form.Group>
-
-        <Form.Group widths='equal'>
-          <Form.Field label='End Date' control={Input} type='date' onChange={handleChange} name='end_date' value={settings.end_date} />
-          <Form.Field label='End Time' control={Input} type='time' onChange={handleChange} name='end_time' value={settings.end_time} />
-        </Form.Group>
-
         <h3>Practice Period</h3>
 
         <Form.Group widths='equal'>
@@ -145,6 +133,18 @@ const Settings = (): JSX.Element => {
         <Form.Group widths='equal'>
           <Form.Field label='End Date' control={Input} type='date' onChange={handleChange} name='practice_end_date' value={settings.practice_end_date} />
           <Form.Field label='End Time' control={Input} type='time' onChange={handleChange} name='practice_end_time' value={settings.practice_end_time} />
+        </Form.Group>
+
+        <h3>Competition Time</h3>
+
+        <Form.Group widths='equal'>
+          <Form.Field label='Start Date' control={Input} type='date' onChange={handleChange} name='start_date' value={settings.start_date} />
+          <Form.Field label='Start Time' control={Input} type='time' onChange={handleChange} name='start_time' value={settings.start_time} />
+        </Form.Group>
+
+        <Form.Group widths='equal'>
+          <Form.Field label='End Date' control={Input} type='date' onChange={handleChange} name='end_date' value={settings.end_date} />
+          <Form.Field label='End Time' control={Input} type='time' onChange={handleChange} name='end_time' value={settings.end_time} />
         </Form.Group>
 
         <h3>Scoring System</h3>
