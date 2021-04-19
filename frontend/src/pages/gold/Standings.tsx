af14d9cc9259f1bd82fb56783e213b126c70d5db
Jack Pfeiffer <jl5her@yahoo.com>
2021-04-19 12:15:01 -0500

    idk

--- frontend/src/pages/gold/Standings.tsx
+++ frontend/src/pages/gold/Standings.tsx
@@ -123,8 +123,8 @@ const Standings = (): JSX.Element => {
               <Table.Cell>{team.score}</Table.Cell>
               {problems.map(problem =>
                 team.problems[problem.pid]?.status == 'accepted' ?
-                  <Table.Cell key={`${team.uid}-${index}`} className={`score ${statusToClass(team.problems[problem.pid].status)}`}>
-                    {team.problems[problem.pid].score}
+                  <Table.Cell key={`${team.uid}-${index}`} className={`score ${statusToClass(team.problems[problem.pid]?.status)}`}>
+                    {team.problems[problem.pid]?.score}
                   </Table.Cell>
                   : <Table.Cell key={`${team.uid}-${index}}`}></Table.Cell>
               )}
