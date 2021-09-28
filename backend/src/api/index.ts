aa66c4936faa617b599740aa598d160730645893
Jack Pfeiffer <jl5her@yahoo.com>
2021-09-28 00:06:58 -0500

    Only show swagger in dev env

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -12,32 +12,38 @@ import swaggerUi from 'swagger-ui-express'
 
 const api = Router()
 
-api.use('/swagger.json', (_req: Request, res: Response) => {
-  res.setHeader('Content-Type', 'application/json')
-  res.send(
-    swaggerJsdoc({
-      swaggerDefinition: {
-        openapi: '3.0.0',
-        info: {
-          title: 'Abacus API',
-          version: '1.0.0',
-          description: 'API for codeabac.us'
-        }
-      },
-      apis: ['**/*.ts', '**/*.d.ts']
+// Only show swagger docs on local environment
+if (process.env.NODE_ENV == 'development') { 
+  api.use('/swagger.json', (_req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(
+      swaggerJsdoc({
+        swaggerDefinition: {
+          openapi: '3.0.0',
+          info: {
+            title: 'Abacus API',
+            version: '1.0.0',
+            description: 'API for codeabac.us'
+          }
+        },
+        apis: ['**/*.ts', '**/*.d.ts']
+      })
+    )
+  })
+
+  api.use(
+    '/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+      swaggerOptions: {
+        url: '/swagger.json'
+      }
     })
   )
-})
-
-api.use(
-  '/docs',
-  swaggerUi.serve,
-  swaggerUi.setup(undefined, {
-    swaggerOptions: {
-      url: '/swagger.json'
-    }
-  })
-)
+  api.get('/', (_, res) => res.redirect('/docs')) // Redirects to docss
+} else {
+  api.get('/', (_, res) => res.send(' ')) // Sends 200 OK when AWS EBS pings server for health check
+}
 
 api.use(auth, problems, clarifications, contest, standings, submissions, users, scratch)
 
