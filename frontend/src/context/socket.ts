d56379ad9a01537aac80b2d2918d0633d77edc07
Kevin O'Connell <kevinoconnell42@gmail.com>
2021-09-29 10:15:52 -0500

    site should compile now

--- frontend/src/context/socket.ts
+++ frontend/src/context/socket.ts
@@ -1,6 +1,6 @@
 import { createContext } from 'react'
 import { Socket } from 'socket.io-client'
 
-const SocketContext = createContext<Socket | undefined>(undefined)
+const SocketContext = createContext<typeof Socket | undefined>(undefined)
 
 export default SocketContext
