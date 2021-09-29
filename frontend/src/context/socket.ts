446c356b4bf4c24fb36766a8566ba07fbba38cb2
Kevin O'Connell <kevinoconnell42@gmail.com>
2021-09-29 15:02:39 -0500

    revert to old socket file

---
+++ frontend/src/context/socket.ts
@@ -0,0 +1,17 @@
+import { Submission, Notification } from 'abacus'
+import { createContext } from 'react'
+import { Socket } from 'socket.io-client'
+
+interface ClientToServerEvents {
+  notification: (notification: Notification) => void
+  new_submission: (submission: Submission) => void
+  update_submission: (submission: Submission) => void
+  delete_submission: (submission: Submission) => void
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ServerToClientEvents {}
+
+const SocketContext = createContext<Socket<ClientToServerEvents, ServerToClientEvents> | undefined>(undefined)
+
+export default SocketContext
