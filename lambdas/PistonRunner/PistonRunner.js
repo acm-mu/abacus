0be8c8051bd9cc7022f6e28e7e2a25b505424105
Jack Pfeiffer <jl5her@yahoo.com>
2021-10-05 00:18:10 -0500

    Piston runner use v3 spec

--- lambdas/PistonRunner/PistonRunner.js
+++ lambdas/PistonRunner/PistonRunner.js
@@ -1,127 +1,120 @@
-const AWS = require("aws-sdk");
-const axios = require("axios");
+const axios = require('axios')
+const { DynamoDB: { Converter: { unmarshall } } } = require('aws-sdk')
 
-const db = new AWS.DynamoDB.DocumentClient();
+const API_URL = "http://localhost"
 
 exports.handler = async(event) => {
-    const submissions = {};
-    for (const record of event.Records) {
-        // Only run for new items
-        if (record.eventName != "INSERT") continue;
 
-        // Find submission from event metadata
-        const submission = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
-        const { sid, pid, division } = submission;
+  // Authenticate to get accessToken
+  const res = await axios.post(`${API_URL}/auth`, {
+    username: 'admin',
+    password: 'goldeneagles'
+  })
 
-        if (division !== 'blue') continue
+  const { accessToken } = res.data
 
-        // Get problem and competition details
-        const problem = await getItem('problem', { pid })
-        const settings = Object.assign({}, ...(await scanItems('setting')).map(((x) => ({
-            [x.key]: x.value
-        }))));
+  const submissions = {}
 
-        // Update status to 'pending'
-        await updateItem('submission', { sid }, { status: 'pending' });
+  for (const record of event.Records) {
+    if (record.eventName != "INSERT") continue
 
-        // Extract details and set defaults
-        const { language, source, date: submission_date } = submission;
-        let status = "accepted";
-        let runtime = -1;
+    const submission = unmarshall(record.dynamodb.NewImage)
+    const { sid, pid, division } = submission
 
-        // Copy tests from problem
-        submission.tests = problem.tests;
+    if (division !== 'blue') continue
 
-        // Run tests
-        for (const test of submission.tests) {
-            // Await response from piston execution
-            const res = await axios.post("https://piston.codeabac.us/execute", {
-                language,
-                source,
-                stdin: test.in
-            }, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
+    // Get problem and competition details
+    let problem, settings
+    try {
+      const problemRes = await axios.get(`${API_URL}/problems?pid=${pid}&columns=tests`, {
+        headers: { Authorization: `Bearer ${accessToken}` }
+      })
 
-            runtime = Math.max(runtime, res.data.runtime);
-            test.stdout = res.data.output;
+      problem = problemRes.data[pid]
 
-            if (res.data.output != test.out) {
-                console.log("Result: REJECTED");
-                status = "rejected";
-                test['result'] = "rejected";
-            } else {
-                console.log("Result: ACCEPTED");
-                test['result'] = "accepted";
-            }
-        }
-
-        submission.status = status
-        submission.runtime = runtime
-
-        // Calculate Score
-        if (status == "accepted") {
-            let { start_date, points_per_no, points_per_yes, points_per_minute } = settings;
-            if (problem.practice) {
-                start_date = settings.practice_start_date
-            }
-            const minutes = (submission_date - start_date) / 60;
-
-            submission.score = Math.floor((minutes * points_per_minute) + (points_per_no * submission.sub_no) + points_per_yes);
-        } else {
-            submission.score = 0;
-        }
-
-        // Save submission to database
-        await updateItem('submission', { sid }, {...submission });
-
-        submissions[sid] = submission;
+      const settingsRes = await axios.get(`${API_URL}/contest`)
+      settings = settingsRes.data
+    } catch (e) {
+      console.log("Error occurred while trying to get submission info")
     }
 
-    return { statusCode: 200, submissions };
-};
+    // Update status to pending
+    try {
+      await axios.put(`${API_URL}/submissions`, {
+        sid: submission.sid,
+        status: "pending"
+      }, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+    } catch (e) {
+      console.log("Error occurred while trying to update submission")
+    }
 
-function scanItems(tableName) {
-    return new Promise((resolve, reject) => {
-        db.scan({
-            TableName: tableName
-        }, (err, data) => {
-            if (err) reject(err);
-            else resolve(data.Items);
-        });
-    });
-}
+    // Extract details and set defaults
+    const { language, source, date: submission_date } = submission
+    let status = "accepted"
+    let runtime = -1
 
-function getItem(tableName, key) {
-    return new Promise((resolve, reject) => {
-        db.get({
-            TableName: tableName,
-            Key: key
-        }, (err, data) => {
-            if (err) reject(err);
-            else resolve(data.Item);
-        });
-    });
-}
+    // Copy tests from problem
+    submission.tests = problem.tests
 
-function updateItem(tableName, key, args) {
-    return new Promise((resolve, reject) => {
-        const entries = Object.entries(args).filter(entry => !Object.keys(key).includes(entry[0]));
-        db.update({
-            TableName: tableName,
-            Key: key,
-            UpdateExpression: "SET " + (entries.map((e) => (`#${e[0]} = :${e[0]}`)).join(", ")),
-            ExpressionAttributeNames: Object.assign({}, ...entries.map((x) => ({
-                [`#${x[0]}`]: x[0]
-            }))),
-            ExpressionAttributeValues: Object.assign({}, ...entries.map((x) => ({
-                [`:${x[0]}`]: x[1]
-            })))
-        }, (err, data) => {
-            if (err) reject(err);
-            else resolve(data);
-        });
-    });
+    // Run tests
+    for (const test of submission.tests) {
+      // Await response from piston execution
+      const res = await axios.post(`${API_URL}:2000/api/v2/execute`, {
+        language,
+        version: "15.0.2",
+        files: [{ content: source }],
+        stdin: test.in
+      }, {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+
+      // runtime = Math.max(runtime, res.data.runtime)
+      test.stdout = res.data.run.output
+
+      if (res.data.output != test.out) {
+        console.log("Result: REJECTED")
+        status = "rejected"
+        test['result'] = "rejected"
+      } else {
+        console.log("Result: ACCEPTED")
+        test['result'] = "accepted"
+      }
+    }
+
+    submission.status = status
+    submission.runtime = runtime
+
+    // Calculate score
+    if (status == "accepted") {
+      let { start_date, points_per_no, points_per_yes, points_per_minute } = settings
+      if (problem.practice) {
+        start_date = settings.practice_start_date
+      }
+      const minutes = (submission_date - start_date) / 60
+
+      submission.score = Math.floor((minutes * points_per_minute) + (points_per_no * submission.sub_no) + points_per_yes)
+    } else {
+      submission.score = 0
+    }
+
+    // Save submission to database
+    try {
+      await axios.put(`${API_URL}/submissions`, submission, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+    } catch (e) {
+      console.log("Error occurred while trying to save submission")
+    }
+    submissions[sid] = submission
+  }
+
+  return { statusCode: 200, submissions }
 }
