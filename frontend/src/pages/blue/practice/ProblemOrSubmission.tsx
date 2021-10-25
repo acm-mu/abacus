7e9011b1d72ca3b7be21fb0ef62501443382c075
Jl5her <Jl5her@users.noreply.github.com>
2021-10-25 19:16:15 +0000

    Prettified Code!

--- frontend/src/pages/blue/practice/ProblemOrSubmission.tsx
+++ frontend/src/pages/blue/practice/ProblemOrSubmission.tsx
@@ -10,7 +10,9 @@ const ProblemOrSubmission = (): JSX.Element => {
   const [isMounted, setMounted] = useState(true)
   const [isLoading, setLoading] = useState(true)
   const [problems, setProblems] = useState<Problem[]>([])
-  const submissions: { [key: string]: Submission } = localStorage.submissions ? JSON.parse(localStorage.submissions) : {}
+  const submissions: { [key: string]: Submission } = localStorage.submissions
+    ? JSON.parse(localStorage.submissions)
+    : {}
 
   const { id } = useParams<{ id: string }>()
 
