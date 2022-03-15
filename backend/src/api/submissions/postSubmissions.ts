9f940dabc9b20c50cc4a04124fb4368630009e7b
Kevin O'Connell <kevinoconnell42@gmail.com>
2022-03-15 15:23:27 -0500

    submissions now connected to mongodb

--- backend/src/api/submissions/postSubmissions.ts
+++ backend/src/api/submissions/postSubmissions.ts
@@ -36,7 +36,6 @@ export const schema: Record<string, ParamSchema> = {
     notEmpty: true,
     optional: true
   },
-
   design_document: {
     in: 'body',
     isString: true,
@@ -87,84 +86,10 @@ export const schema: Record<string, ParamSchema> = {
  *       500:
  *         description: A server error occured while trying to complete request.
  * 
- * const submissions = {};
-    for (const record of event.Records) {
-        // Only run for new items
-        if (record.eventName != "INSERT") continue;
-
-        // Find submission from event metadata
-        const submission = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
-        const { sid, pid, division } = submission;
-
-        if (division !== 'blue') continue
-
-        // Get problem and competition details
-        const problem = await getItem('problem', { pid })
-        const settings = Object.assign({}, ...(await scanItems('setting')).map(((x) => ({
-            [x.key]: x.value
-        }))));
-
-        // Update status to 'pending'
-        await updateItem('submission', { sid }, { status: 'pending' });
-
-        // Extract details and set defaults
-        const { language, source, date: submission_date } = submission;
-        let status = "accepted";
-        let runtime = -1;
-
-        // Copy tests from problem
-        submission.tests = problem.tests;
-
-        // Run tests
-        for (const test of submission.tests) {
-            // Await response from piston execution
-            const res = await axios.post("https://piston.codeabac.us/execute", {
-                language,
-                source,
-                stdin: test.in
-            }, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-
-            runtime = Math.max(runtime, res.data.runtime);
-            test.stdout = res.data.output;
-
-            if (res.data.output != test.out) {
-                console.log("Result: REJECTED");
-                status = "rejected";
-                test['result'] = "rejected";
-            } else {
-                console.log("Result: ACCEPTED");
-                test['result'] = "accepted";
-            }
-        }
-
-        submission.status = status
-        submission.runtime = runtime
-
-        // Calculate Score
-        if (status == "accepted") {
-            let { start_date, points_per_no, points_per_yes, points_per_minute } = settings;
-            if (problem.practice) {
-                start_date = settings.practice_start_date
-            }
-            const minutes = (submission_date - start_date) / 60;
-
-            submission.score = Math.floor((minutes * points_per_minute) + (points_per_no * submission.sub_no) + points_per_yes);
-        } else {
-            submission.score = 0;
-        }
-
-        // Save submission to database
-        await updateItem('submission', { sid }, {...submission });
-
-        submissions[sid] = submission;
-    }
-
-    return { statusCode: 200, submissions };
- */
+ *
+ * 
+ */ 
+     
 
 export const postSubmissions = async (req: Request, res: Response): Promise<void> => {
   const errors = validationResult(req).array()
@@ -260,7 +185,6 @@ export const postSubmissions = async (req: Request, res: Response): Promise<void
       score: 0,
       date: Date.now() / 1000
     }
-    console.log('source', item)
     if (item.division === 'blue') {
       if (req.files?.source == undefined) {
         res.status(400).json({ message: 'source not provided' })
@@ -318,11 +242,7 @@ export const postSubmissions = async (req: Request, res: Response): Promise<void
                   }
                 }
               )
-              console.log('res data', res.data)
-         
               test.stdout = res.data.run.output
-              console.log('output', res.data.run.output)
-              console.log('test out', test.out)
               if (res.data.output != test.out) {
                 console.log('Result: ACCEPTED')
                 test['result'] = 'accepted'
@@ -354,7 +274,7 @@ export const postSubmissions = async (req: Request, res: Response): Promise<void
           submission.score = 0
         }
 
-        // Save submission to databas
+        // Save submission to database
         await contest.update_submission(submission.sid as string, { ...submission, sid: submission.sid })
       } else if (req.user?.division == 'gold') {
         const scratchResponse = await axios.get(`https://api.scratch.mit.edu/projects/${item.project_id}`)
