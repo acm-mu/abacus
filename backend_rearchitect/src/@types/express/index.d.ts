012205cab381277f23a5bcfeadf6176717e60cd5
brad-cooley <brad@cooleyweb.org>
2023-11-01 22:27:39 -0400

    Initial scaffolding of new architecture

--- backend_rearchitect/src/@types/express/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { User } from 'abacus'
-declare global {
-  namespace Express {
-    interface Request {
-      user?: User
-    }
-  }
-}
--- backend_rearchitect/src/services/db/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Database } from './database'
-export { default as MongoDB } from './mongo'
-export { default as DynamoDB } from './dynamo'
