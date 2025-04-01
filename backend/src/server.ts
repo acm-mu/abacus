494f688a13e6f38baa5f6cc4c8f2cf28b2078faa
Cezzzzzz <figueroa.cesar234@gmail.com>
2025-03-31 22:08:29 -0500

    Revert "Https backend fixes"
    This reverts commit bed64ae32539768817f115702712a7e41b174083.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,34 +17,27 @@ const PORT = process.env.PORT || 80
 
 const app = express()
 const server = createServer(app)
-
 export const io = new Server(server, {
   serveClient: false,
   cors: {
-    origin: 'https://abacus.cs.mu.edu', // Ensure frontend origin is allowed
-    methods: ['GET', 'POST']
+    origin: '*'
   },
-  transports: ['websocket', 'polling'], // Added polling as a fallback
-  secure: true
+  transports: ['websocket']
 })
 
 if (process.env.REDIS_HOST) {
-  try {
-    const { REDIS_HOST: host, REDIS_PASS: auth_pass } = process.env
-    const pubClient = new RedisClient({ host, port: 6379, auth_pass })
-    const subClient = pubClient.duplicate()
+  const { REDIS_HOST: host, REDIS_PASS: auth_pass } = process.env
 
-    io.adapter(createAdapter({ pubClient, subClient }))
-  } catch (error) {
-    console.error('Redis connection failed:', error)
-  }
+  const pubClient = new RedisClient({ host, port: 6379, auth_pass })
+  const subClient = pubClient.duplicate()
+
+  io.adapter(createAdapter({ pubClient, subClient }))
 }
 
-app.use(cors()) 
-app.use(express.json()) 
-app.use(fileUpload()) 
-
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+app.use(cors()) // Enables CORS on all endpoints
+app.use(express.json()) // Middleware to parse body of requests as JSON
+app.use(fileUpload()) // Middleware for uploading files to express (accessible in req.files)
+if (process.env.NODE_ENV == 'development') app.use(morgan('dev'))
 
 app.use(api)
 
@@ -53,17 +46,11 @@ export function sendNotification({ header, to, content, type, context }: Notific
 }
 
 io.on('connection', (socket: Socket) => {
-  console.log('WebSocket connected:', socket.id)
-
   socket.on('broadcast', ({ ev, args }) => {
     io.sockets.emit(ev, args)
-  })
-
-  socket.on('disconnect', () => {
-    console.log('WebSocket disconnected:', socket.id)
   })
 })
 
 server.listen(PORT, () => {
-  console.log(`🚀 Server is running at https://abacus.cs.mu.edu`)
+  console.log(`🚀 Server is running at :${PORT}`)
 })
